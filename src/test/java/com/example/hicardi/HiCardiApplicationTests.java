d85900e825067d88b82a2a6d31ae931f7f2e9f05
eojin0814 <leeeojin6@gmail.com>
2023-09-11 17:59:12 +0900

    [init] settings

---
+++ src/test/java/com/example/hicardi/HiCardiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hicardi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiCardiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
