759fbf07e82f8e2a929938f39f09127fdae4df23
eojin0814 <leeeojin6@gmail.com>
2023-09-12 23:05:43 +0900

    [feat] test

---
+++ src/main/java/com/example/hicardi/ApiTestController.java
@@ -0,0 +1,13 @@
+package com.example.hicardi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiTestController {
+    @RequestMapping("/")
+    public String test() {
+        return "test good blue ";
+    }
+
+}
