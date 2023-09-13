fe541beb2d3ecf636d14958f61575c1dec0f2382
emilywin825 <emilywin825@naver.com>
2023-09-13 21:29:44 +0900

    [feat]cart 패키지 생성 / User엔터티와 Cart 엔터티 연관관계 매핑

---
+++ src/main/java/com/example/hicardi/domain/cart/entity/Cart.java
@@ -0,0 +1,27 @@
+package com.example.hicardi.domain.cart.entity;
+
+import com.example.hicardi.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_Id")
+    private Long cartId;
+
+    @Column(name = "quantity")
+    private Long quantity; //수량
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
