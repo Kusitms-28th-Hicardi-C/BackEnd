cf8fc009d3e588f359037c18d9c782f279dec094
eojin0814 <leeeojin6@gmail.com>
2023-09-16 15:45:23 +0900

    [fix] 파일 다운로드 구현

--- src/main/java/com/example/hicardi/domain/FileUpload/controller/S3FileDownloadController.java
+++ src/main/java/com/example/hicardi/domain/FileUpload/controller/S3FileDownloadController.java
@@ -1,12 +1,15 @@
 package com.example.hicardi.domain.FileUpload.controller;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.example.hicardi.domain.FileUpload.service.S3downloadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,23 +21,22 @@ public class S3FileDownloadController {
 
     private final AmazonS3 amazonS3;
 
+    private final S3downloadService s3downloadService;
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
 
 
-    @GetMapping("/test")
-    public ResponseEntity<UrlResource> downloadImage(String originalFilename) {
-
-        originalFilename = "aws test.txt.txt";
-        UrlResource urlResource = new UrlResource(amazonS3.getUrl(bucket, originalFilename));
-
-        String contentDisposition = "attachment; filename=\"" +  originalFilename + "\"";
-
-        // header에 CONTENT_DISPOSITION 설정을 통해 클릭 시 다운로드 진행
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
-                .body(urlResource);
-
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = s3downloadService.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
     }
 }
